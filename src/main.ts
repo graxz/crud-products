75d0acecf0c39d16b1347982ff8e6291254c0860
Isaddora Freitas <isaddorafreitasar@gmail.com>
2021-12-07 13:23:24 -0300

    port adjust

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(process.env.SERVER_PORT || 3000);
 }
 
 bootstrap();
